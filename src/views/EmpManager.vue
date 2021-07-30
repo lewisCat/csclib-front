cdc86ba7bc9bd040315994c4a17a6f714a714a50
lewis <xylewis@126.com>
2021-07-30 17:36:19 +0800

    人员管理完成分页查询,编辑功能完善中,条件搜索未完成

--- src/views/EmpManager.vue
+++ src/views/EmpManager.vue
@@ -46,12 +46,19 @@
 					<el-table-column label="所属公司" prop="company"> </el-table-column>
 					<el-table-column label="入职日期" prop="entryday"> </el-table-column>
 					<el-table-column label="离职日期" prop="outday"> </el-table-column>
+					<el-table-column label="员工性质" prop="status"> </el-table-column>
 
 					<el-table-column align="right">
 						<template #default="scope">
 							<el-button
 								type="text"
 								icon="el-icon-edit"
+								@click="addCertificate(scope.$index, scope.row)"
+								>证书</el-button
+							>
+							<el-button
+								type="text"
+								icon="el-icon-notebook-1"
 								@click="handleEdit(scope.$index, scope.row)"
 								>编辑</el-button
 							>
@@ -76,12 +83,12 @@
 					layout="prev, pager, next"
 					:total="cutPage.count"
 					:page-size="cutPage.size"
-					:current-page="cutPage.current"
+					:current-page.sync="cutPage.current"
+					@current-change="currentQuery"
 				>
 				</el-pagination>
 			</div>
 		</div>
-
 		<!-- 新增员工表单-->
 		<el-dialog title="新增员工" center v-model="showAddemp" width="40%">
 			<div class="add-emp-form">
@@ -123,6 +130,17 @@
 							</el-option>
 						</el-select>
 					</el-form-item>
+					<el-form-item label="员工性质" prop="status">
+						<el-select v-model="employe.status" placeholder="请选择员工性质" style="width: 100%">
+							<el-option
+								v-for="item in statues"
+								:key="item.value"
+								:value="item.value"
+								:label="item.title"
+							>
+							</el-option>
+						</el-select>
+					</el-form-item>
 					<el-form-item label="入职日期" prop="entryday">
 						<el-date-picker
 							v-model="employe.entryday"
@@ -142,11 +160,86 @@
 				</el-form>
 			</div>
 		</el-dialog>
+		<!-- 修改员工表单 -->
+		<el-dialog title="修改员工信息" center v-model="showModifyemp" width="40%">
+			<div class="add-emp-form">
+				<el-form :model="employe" :rules="rules" label-width="80px" :inline="false" size="small">
+					<el-form-item label="员工姓名" prop="name">
+						<el-input v-model="employe.name"></el-input>
+					</el-form-item>
+					<el-form-item label="身份证号" prop="sid">
+						<el-input
+							v-model="employe.sid"
+							maxlength="18"
+							minlength="16"
+							show-word-limit
+						></el-input>
+					</el-form-item>
+					<el-form-item label="联系电话" prop="phonenum">
+						<el-input
+							v-model="employe.phonenum"
+							maxlength="11"
+							minlength="11"
+							show-word-limit
+						></el-input>
+					</el-form-item>
+					<el-form-item label="所属公司" prop="company">
+						<el-select v-model="employe.company" placeholder="请选择所属公司" style="width: 100%">
+							<el-option
+								v-for="item in companysinfo"
+								:key="item.value"
+								:value="item.value"
+								:label="item.company"
+							>
+							</el-option>
+						</el-select>
+					</el-form-item>
+					<el-form-item label="员工性质" prop="status">
+						<el-select v-model="employe.status" placeholder="请选择员工性质" style="width: 100%">
+							<el-option
+								v-for="item in statues"
+								:key="item.value"
+								:value="item.value"
+								:label="item.title"
+							>
+							</el-option>
+						</el-select>
+					</el-form-item>
+					<el-form-item label="离职日期">
+						<el-date-picker
+							v-model="employe.outDay"
+							type="date"
+							placeholder="入职日期"
+							style="width: 100%"
+							value-format="YYYY-MM-DD"
+						>
+						</el-date-picker>
+					</el-form-item>
+					<el-form-item label="入职日期">
+						<el-date-picker
+							v-model="employe.entryday"
+							type="date"
+							placeholder="入职日期"
+							style="width: 100%"
+							value-format="YYYY-MM-DD"
+						>
+						</el-date-picker>
+					</el-form-item>
+					<el-form-item>
+						<div style="text-align: center">
+							<el-button type="primary" @click="modifyemp">修改</el-button>
+							<el-button @click="showModifyemp = false">取消</el-button>
+						</div>
+					</el-form-item>
+				</el-form>
+			</div>
+		</el-dialog>
+		<!--新增人员证书-->
 	</div>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive, ref, triggerRef } from "vue";
+import { defineComponent, onBeforeMount, reactive, ref, triggerRef, watchEffect } from "vue";
 import request from "@/plugins/request";
 import { ElMain, ElMessage } from "element-plus";
 import axios from "axios";
@@ -155,6 +248,7 @@ import datefmt from "@/plugins/dateFormate";
 export default defineComponent({
 	name: "EmpManager",
 	setup() {
+		//员工数据模型
 		const employe = reactive({
 			id: "",
 			name: "",
@@ -165,22 +259,32 @@ export default defineComponent({
 			outday: "",
 			company: "",
 		});
-
+		//分页模型
 		const cutPage = reactive({
 			current: 1,
-			size: 5,
-			count: 10,
+			size: 8,
+			count: 0,
 		});
-
+		//搜索关键字
 		const searchKeyWord = reactive({
-			keyWord: "",
-			company: "",
+			seName: "",
+			seCompany: "",
 		});
-		const searchCondition = ref("name");
+		//员工性质模型
+		const statues = reactive([
+			{
+				title: "内部员工",
+				value: 0,
+			},
+			{
+				title: "外部员工",
+				value: 1,
+			},
+		]);
 		const showAddemp = ref(false);
 		const addempFrom = ref(null);
-
 		const employese = reactive([]);
+		const showModifyemp = ref(false);
 		//公司信息
 		const companysinfo = [
 			{
@@ -194,7 +298,6 @@ export default defineComponent({
 		];
 		//搜索员工信息
 		const searchEmp = () => {};
-
 		//新增员工按钮事件
 		const openAddemp = () => {
 			showAddemp.value = true;
@@ -202,31 +305,33 @@ export default defineComponent({
 				addempFrom.value.resetFields();
 			}
 		};
-
-		//获取员工信息
-		const getEmpInfos = () => {
-			axios
-				.all([
-					request.get("/emp/count"),
-					request.get("/emp/list", { params: { current: cutPage.current, size: cutPage.size } }),
-				])
-				.then(
-					axios.spread((res1, res2) => {
-						cutPage.count = res1.data;
-						res2.data.forEach(element => {
-							element.entryday = datefmt(element.entryday);
-							if (element.outday == "" || !element.outday) {
-								element.outday = "无";
-							} else {
-								element.outday = datefmt(element.outday);
-							}
-							employese.push(element);
-						});
-					})
-				);
+		//获取员工信息列表
+		const getEmpInfos = (current, size) => {
+			employese.length = 0;
+			request.get("/emp/list", { params: { current: current, size: size } }).then(res => {
+				res.data.forEach(element => {
+					element.entryday = datefmt(element.entryday);
+					if (element.outday == "" || !element.outday) {
+						element.outday = "无";
+					} else {
+						element.outday = datefmt(element.outday);
+					}
+					if (element.status == 0) {
+						element.status = "内部员工";
+					} else {
+						element.status = "外部员工";
+					}
+					employese.push(element);
+				});
+			});
 		};
-
-		getEmpInfos();
+		//页面挂在前初始化员工数据
+		onBeforeMount(() => {
+			request.get("/emp/count").then(res => {
+				cutPage.count = res.data;
+			});
+			getEmpInfos(cutPage.current, cutPage.size);
+		});
 		// 新增员工
 		const addemp = () => {
 			addempFrom.value.validate(vali => {
@@ -247,10 +352,51 @@ export default defineComponent({
 				}
 			});
 		};
+		//编辑
+		const handleEdit = (index, row) => {
+			showModifyemp.value = true;
+			employe.id = row.id;
+			employe.name = row.name;
+			employe.sid = row.sid;
+			employe.phonenum = row.phonenum;
+		};
+		// 删除
+		const handleDelete = (index, row) => {
+			//判断员工是否存在证书信息
+			request.get("/certificates/qbempid", { params: { empid: row.id } }).then(res => {
+				if (res.data.length != 0) {
+					//员工存在证书信息先删除员工证书信息
+					request.get("/certificates/del", { params: { empid: row.id } }).then(res => {
+						if (!res.data) {
+							ElMessage.error("员工信息删除失败,请检查员工证书信息");
+							return;
+						}
+					});
+				}
+			});
+			//删除员工信息
+			request.get("/emp/del", { params: { id: row.id } }).then(res => {
+				if (res.data) {
+					ElMessage.success("员工信息删除成功");
+					employese.splice(index, 1);
+				} else {
+					ElMessage.error("员工信息删除失败,请检查员工证书信息");
+				}
+			});
+		};
+		//分页按钮事件
+		const currentQuery = current => {
+			cutPage.current = current;
+			getEmpInfos(cutPage.current, cutPage.size);
+		};
 
-		const handleEdit = (index, row) => {};
-
-		const handleDelete = (index, row) => {};
+		//修改员工事件
+		const modifyemp = () => {
+			
+		};
+		
+	//新增员工证书
+		const addCertificate=(index,row) => {}
 
 		//#region 表单数据校验
 		// 身份证号码校验
@@ -259,17 +405,18 @@ export default defineComponent({
 				callback(new Error("身份证号码不能为空"));
 				return;
 			}
-			console.log(value);
-
-			request.post("/emp/qbsid", value).then(res => {
+			if (value.length != 18) {
+				callback(new Error("身份证号码少于18位"));
+				return;
+			}
+			request.post("/emp/cksid", "sid=".concat(value)).then(res => {
 				if (res.data) {
 					callback(new Error("该身份证员工已存在,请检查身份证信息"));
-				} else {
-					callback();
+					return;
 				}
 			});
+			callback();
 		};
-		//电话号码校验
 
 		const rules = {
 			name: {
@@ -297,25 +444,34 @@ export default defineComponent({
 				message: "请选择员工所属公司",
 				trgger: "change",
 			},
+			status: {
+				required: true,
+				message: "请选择员工性质",
+				trgger: "change",
+			},
 		};
 		//#endregion
 
 		return {
 			employe,
 			employese,
-			searchCondition,
 			searchKeyWord,
 			cutPage,
 			addempFrom,
 			rules,
+			statues,
 			showAddemp,
 			companysinfo,
+			showModifyemp,
 
+			currentQuery,
 			addemp,
 			handleEdit,
 			handleDelete,
 			openAddemp,
 			searchEmp,
+			modifyemp,
+			addCertificate
 		};
 	},
 });
